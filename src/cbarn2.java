f4a27c1448fe5a36aa127c3ec8a2711d20d046ae
abx393 <abandari99@gmail.com>
2020-11-14 17:45:04 -0800

    style

--- src/cbarn2.java
+++ src/cbarn2.java
@@ -17,24 +17,24 @@ public class cbarn2 {
 		for (int i=0; i<n; i++) orig[i] = a[i];
 		
 		long res = Long.MAX_VALUE;
-		for (int start = 0; start<n; start++){ 
+		for (int start = 0; start<n; start++) { 
 			for (int i=0; i<n; i++) Arrays.fill(dp[i], Long.MAX_VALUE);
 			for (int i=0; i<n; i++) a[i] = orig[(start+i)%n];
 			
 			long sum = 0;
-			long ps = 0; //prefix sum
+			long ps = 0; // prefix sum
 			for (int i=n-1; i>=0; i--){
 				dp[i][0] = ps;
 				sum += a[i];
 				ps += sum;
 			}
 			
-			for (int i=1; i<k; i++){
-				for (int j=n-1; j>0; j--){
+			for (int i=1; i<k; i++) {
+				for (int j=n-1; j>0; j--) {
 					sum = 0;
 					ps = 0;
-					for (int b=j-1; b>=0; b--){
-						dp[b][i] = Math.min(dp[b][i], dp[j][i-1]+ps);
+					for (int b=j-1; b>=0; b--) {
+						dp[b][i] = Math.min(dp[b][i], dp[j][i-1] + ps);
 						sum += a[b];
 						ps += sum;
 					}
--- src/twofoureight.java
+++ src/twofoureight.java
@@ -5,6 +5,7 @@ public class twofoureight {
 	public static int[] nums;
 	public static int[][] dp;
 	public static int max = 0; 
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("248.in"));
 		int n = Integer.parseInt(br.readLine());
@@ -20,14 +21,15 @@ public class twofoureight {
 		out.println(max);
 		out.close();
 	}
+
 	public static int solve(int in, int out){
-		if (in==out) return nums[in];
-		if (dp[in][out]!=-1) return dp[in][out];
+		if (in == out) return nums[in];
+		if (dp[in][out] != -1) return dp[in][out];
 		for (int i=in; i<out; i++){
 			int x = solve(in, i);
 			int y = solve(i+1, out);
-			if (x==0 || y==0) continue;
-			if (x!=y) continue;
+			if (x == 0 || y == 0) continue;
+			if (x != y) continue;
 			dp[in][out] = x+1;
 			max = Math.max(max, x+1);
 			return x+1;
